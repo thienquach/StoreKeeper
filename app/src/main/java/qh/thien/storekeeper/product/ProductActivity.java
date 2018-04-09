1117952f1d20d9b69e591d7fd46c72bab70c3efc
Thien Quach <thienqh13@gmail.com>
2018-04-09 22:53:42 +0700

    add StoreKeeperApp as Android application, init saveProduct button

--- app/src/main/java/qh/thien/storekeeper/product/ProductActivity.java
+++ app/src/main/java/qh/thien/storekeeper/product/ProductActivity.java
@@ -2,14 +2,41 @@ package qh.thien.storekeeper.product;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import qh.thien.storekeeper.R;
+import qh.thien.storekeeper.StoreKeeperApp;
 
 public class ProductActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+
+        initSaveProductButton();
+    }
+
+    private String getTextFromEditText(int id){
+        EditText editText = (EditText) findViewById(id);
+        return editText.getText().toString();
+    }
+
+    private void initSaveProductButton() {
+        Button saveProductBtn = (Button) findViewById(R.id.act_product_btn_save);
+        saveProductBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Product productTobeSaved = new Product();
+                productTobeSaved.setName(getTextFromEditText(R.id.act_product_et_product_name));
+                productTobeSaved.setSellingPrice(Double.parseDouble(getTextFromEditText(R.id.act_product_et_product_selling_price)));
+                productTobeSaved.setStockPrice(Double.parseDouble(getTextFromEditText(R.id.act_product_et_product_stock_price)));
+
+                ((StoreKeeperApp)getApplication()).getDataBase().productDao().insert(productTobeSaved);
+            }
+        });
     }
 }
